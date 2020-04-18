9fed0ddfb2a85ebfb6a4139c544168246306703d
wassaf <wassafshahzad@gmail.com>
2020-04-18 14:11:37 -0700

    deleted unessacery files

--- design_patterns/__init__.py
+++ design_patterns/__init__.py
@@ -1,1 +1,16 @@
-from .singleton import singleton
+
+
+
+# A simple decorator to turn your class into a singleton.
+# Making it thread safe  and all that jazz.
+from functools import wraps
+def singleton(f):
+    _instance = None
+
+    @wraps(f)
+    def inner(*args, **kwargs):
+        nonlocal _instance
+        _instance = f(*args, **kwargs) if _instance is None else _instance
+        return _instance
+
+    return inner
--- setup.py
+++ setup.py
@@ -4,15 +4,15 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="design-patterns-Wassaf-Shahzad", # Replace with your own username
-    version="0.0.1",
+    name="design-patterns", # Replace with your own username
+    version="0.0.4",
     author="Wassaf Shahzad",
     author_email="wassafshahzad@gmail.com",
     description="A small python module containing decorators for implementing design patterns",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/wassafshahzad/DesignPatterns.git",
-    packages=[],
+    packages = setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
